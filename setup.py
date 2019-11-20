0349bc365fbde6e936a5e5f724e2af5775e15321
Andrew Sledge <andrew.sledge@coxautoinc.com>
2019-11-19 21:07:38 -0500

    Dropped support for Python 2.7. Good bye old friend.

--- setup.py
+++ setup.py
@@ -23,9 +23,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
     keywords='slack api'
